bc2f7e6cf61300785d0423f3c75d8dcac03f72a1
Emilien Mantel <emilien.mantel@gmail.com>
2016-03-31 18:07:45 +0200

    WIP debian packaging

---
+++ debian/netdata.default.ex
@@ -0,0 +1,10 @@
+# Defaults for netdata initscript
+# sourced by /etc/init.d/netdata
+# installed at /etc/default/netdata by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
